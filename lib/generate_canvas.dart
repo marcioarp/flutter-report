0ac17266b9c685bf80dc44ac40678281a3c18b44
Márcio Rossato <marcioarp@gmail.com>
2020-11-16 18:26:22 -0300

    close  #1

---
+++ lib/generate_canvas.dart
@@ -0,0 +1,84 @@
+library generate_canvas;
+
+import 'package:flutter/material.dart';
+
+class GenerateCanvas extends CustomPainter {
+  var _rel;
+  double _zoom;
+  GenerateCanvas(var rel, double zoom) {
+    _rel = rel;
+    _zoom = zoom;
+  }
+
+  @override
+  void paint(Canvas canvas, Size size) {
+    for (var obj in _rel) {
+      var paint = Paint();
+
+      if (obj['type'] == 'text') {
+        //print(obj);
+
+        final textStyle =
+            TextStyle(color: Colors.black, fontSize: obj["height"] * _zoom);
+        final textSpan = TextSpan(
+          text: obj["text"],
+          style: textStyle,
+        );
+        final textPainter = TextPainter(
+          text: textSpan,
+          textDirection: TextDirection.ltr,
+        );
+        textPainter.layout(minWidth: 0, maxWidth: size.width * _zoom);
+        final offset = Offset(
+            obj["left"].toDouble() * _zoom, obj["top"].toDouble() * _zoom);
+        textPainter.paint(canvas, offset);
+      }
+
+      if (obj['type'] == 'line') {
+        var path = Path();
+        paint.color = Color.fromRGBO(
+            obj['colorRGB'][0], obj['colorRGB'][1], obj['colorRGB'][2], 1);
+        paint.style = PaintingStyle.stroke;
+        paint.strokeWidth = 1;
+
+        path.moveTo(obj["from"]["x"] * _zoom, obj["from"]["y"] * _zoom);
+
+        path.lineTo(obj["to"]["x"] * _zoom, obj["to"]["y"] * _zoom);
+        canvas.drawPath(path, paint);
+      }
+
+      if (obj['type'] == 'rect') {
+        if (obj["fill"]) {
+          paint.color = Color.fromRGBO(obj['fillColorRGB'][0],
+              obj['fillColorRGB'][1], obj['fillColorRGB'][2], 1);
+          paint.style = PaintingStyle.fill;
+          var rect = Rect.fromLTRB(
+              obj["from"]["x"] * _zoom,
+              obj["from"]["y"] * _zoom,
+              obj["to"]["x"] * _zoom,
+              obj["to"]["y"] * _zoom);
+
+          canvas.drawRect(rect, paint);
+        }
+        if (obj["borderWidth"] > 0) {
+          paint.color = Color.fromRGBO(obj['borderColorRGB'][0],
+              obj['borderColorRGB'][1], obj['borderColorRGB'][2], 1);
+          paint.style = PaintingStyle.stroke;
+          var rect = Rect.fromLTRB(
+              obj["from"]["x"] * _zoom,
+              obj["from"]["y"] * _zoom,
+              obj["to"]["x"] * _zoom,
+              obj["to"]["y"] * _zoom);
+
+          canvas.drawRect(rect, paint);
+          paint.strokeWidth = 1;
+        }
+      }
+    }
+  }
+
+  @override
+  bool shouldRepaint(CustomPainter oldDelegate) {
+    return true;
+  }
+}
--- lib/report.dart
+++ lib/report.dart
@@ -27,6 +27,7 @@ class RPPadding {
 class Report {
   var _layout;
   var _data;
+  String _version;
   int _currData = 0;
   setData(var data) {
     _data = data;
@@ -36,58 +37,215 @@ class Report {
     _layout = layout;
   }
 
-  dynamic generate() {
+  dynamic prepare() {
     var ret = [];
     double incTop = 0;
     double incLeft = 0;
+
     if (_layout.length > 1) {
     } else {
       for (var pg in _layout) {
+        _version = pg['version'];
         incTop = pg['margin']['top'].toDouble();
         incLeft = pg['margin']['left'].toDouble();
         ret.addAll(_processBorder(
-            pg["border"], pg["backgroundRGB"], pg["fillBackground"]));
+          pg["border"],
+          pg["backgroundRGB"],
+          pg["fillBackground"],
+          incTop,
+          incLeft,
+          (pg["width"] - pg["margin"]["left"] - pg["margin"]["right"])
+              .toDouble(),
+          (pg["height"] - pg["margin"]["top"] - pg["margin"]["bottom"])
+              .toDouble(),
+        ));
+
         for (var bd in pg["bands"]) {
-          incTop += bd['margin']['top'];
-          incLeft += bd['margin']['left'];
-          ret.addAll(_processBorder(
-              bd["border"], bd["backgroundRGB"], bd["fillBackground"]));
+          incTop +=
+              bd['margin']['top'].toDouble() + pg["padding"]["top"].toDouble();
+          incLeft += bd['margin']['left'].toDouble() +
+              pg["padding"]["left"].toDouble();
+
           if (bd["type"] == "data") {
             for (var dt in _data) {
-              ret.addAll(_processOBJs(
-                  bd["children"], incTop, incLeft, _data[_currData]));
+              ret.addAll(_processBorder(
+                bd["border"],
+                bd["backgroundRGB"],
+                bd["fillBackground"],
+                incTop,
+                incLeft,
+                (pg["width"] -
+                        pg["margin"]["left"] -
+                        pg["margin"]["right"] -
+                        bd["margin"]["left"] -
+                        bd["margin"]["right"] -
+                        pg["padding"]["left"] -
+                        pg["padding"]["right"])
+                    .toDouble(),
+                (bd["height"]).toDouble(),
+              ));
+
+              ret.addAll(_processOBJs(bd["children"], incTop, incLeft, dt));
               _currData++;
-              incTop += bd["height"];
+              incTop += bd["height"].toDouble();
             }
           } else {
+            ret.addAll(_processBorder(
+              bd["border"],
+              bd["backgroundRGB"],
+              bd["fillBackground"],
+              incTop,
+              incLeft,
+              (pg["width"] -
+                      pg["margin"]["left"] -
+                      pg["margin"]["right"] -
+                      bd["margin"]["left"] -
+                      bd["margin"]["right"] -
+                      pg["padding"]["left"] -
+                      pg["padding"]["right"])
+                  .toDouble(),
+              (bd["height"]).toDouble(),
+            ));
+
             ret.addAll(_processOBJs(
                 bd["children"], incTop, incLeft, _data[_currData]));
-            incTop += bd["height"];
+            incTop +=
+                bd["height"].toDouble() + bd['margin']["bottom"].toDouble();
           }
           //decrease to not affect the next band
-          incLeft -= bd['margin']['left'];
+          incLeft -= bd['margin']['left'].toDouble() -
+              pg["padding"]["left"].toDouble();
         }
       }
     }
-    print(ret);
+    return ret;
   }
 
-  dynamic _processBorder(var border, var colorRGB, bool fillBackground) {}
+  dynamic _processBorder(var border, var colorRGB, bool fillBackground,
+      double incTop, double incLeft, double width, double height) {
+    var ret = [];
+    if (border == null) return ret;
+    bool box = _borderIsRegularBox(border);
 
-  List _processOBJs(var objs, double incTop, double incLeft, var currData) {
+    if (fillBackground) {
+      if (box) {
+        ret.add({
+          "type": "rect",
+          "borderStyle": border["style"],
+          "borderColorRGB": border["colorRGB"],
+          "fill": true,
+          "fillColorRGB": colorRGB,
+          "borderWidth": border["top"],
+          "from": {"x": incLeft, "y": incTop},
+          "to": {"x": incLeft + width, "y": incTop + height},
+          "rounded": border["roundend"]
+        });
+        return ret;
+      } else {
+        ret.add({
+          "type": "rect",
+          "borderStyle": border["style"],
+          "borderColorRGB": [0, 0, 0],
+          "fill": true,
+          "fillColorRGB": colorRGB,
+          "borderWidth": 0,
+          "from": {"x": incLeft, "y": incTop},
+          "to": {"x": incLeft + width, "y": incTop + height},
+          "rounded": border["roundend"]
+        });
+      }
+      //print(ret);
+    }
+
+    if (box) {
+      ret.add({
+        "type": "rect",
+        "borderStyle": border["style"],
+        "borderColorRGB": border["colorRGB"],
+        "fill": false,
+        "fillColorRGB": [0, 0, 0],
+        "borderWidth": border["top"],
+        "from": {"x": incLeft, "y": incTop},
+        "to": {"x": incLeft + width, "y": incTop + height},
+        "rounded": border["roundend"]
+      });
+    } else {
+      if (border["top"] > 0) {
+        ret.add({
+          "type": "line",
+          "style": border["style"],
+          "width": border["top"],
+          "colorRGB": border["colorRGB"],
+          "from": {"x": incLeft, "y": incTop},
+          "to": {"x": incLeft + width, "y": incTop},
+        });
+      }
+      if (border["bottom"] > 0) {
+        ret.add({
+          "type": "line",
+          "style": border["style"],
+          "colorRGB": border["colorRGB"],
+          "width": border["bottom"],
+          "from": {"x": incLeft, "y": incTop + height},
+          "to": {"x": incLeft + width, "y": incTop + height},
+        });
+      }
+      if (border["left"] > 0) {
+        ret.add({
+          "type": "line",
+          "style": border["style"],
+          "colorRGB": border["colorRGB"],
+          "width": border["left"],
+          "from": {"x": incLeft, "y": incTop},
+          "to": {"x": incLeft, "y": incTop + height},
+        });
+      }
+      if (border["right"] > 0) {
+        ret.add({
+          "type": "line",
+          "style": border["style"],
+          "width": border["right"],
+          "colorRGB": border["colorRGB"],
+          "from": {"x": incLeft + width, "y": incTop},
+          "to": {"x": incLeft + width, "y": incTop + height},
+        });
+      }
+    }
+    return ret;
+  }
+
+  /**
+   * Check if border is a regular box, with all sides same width.
+   */
+  bool _borderIsRegularBox(var border) {
+    if (border["top"] <= 0) return false;
+    bool ret = (border["top"] == border["left"]) ==
+        (border["bottom"] == border["right"]);
+
+    return ret;
+  }
+
+  List _processOBJs(var objs, double incTop, double incLeft, var data) {
     List ret = [];
     if (objs == null) return ret;
     for (var obj in objs) {
       if (obj['type'] == 'panel') {
         ret.addAll(_processOBJs(
             obj["children"],
-            incTop + obj["margin"]["top"] + obj["padding"]["top"],
-            incLeft + obj["margin"]["left"] + obj["padding"]["left"],
-            currData));
+            incTop + obj["margin"]["top"] + obj["padding"]["top"] + obj["top"],
+            incLeft +
+                obj["margin"]["left"] +
+                obj["padding"]["left"] +
+                obj["left"],
+            data));
       }
       if (obj['type'] == 'text') {
-        var objRet = obj;
-        objRet['text'] = _processText(obj["text"], currData);
+        var objRet = new Map.from(obj);
+        objRet['text'] = _processText(obj["text"], data);
+        objRet['top'] += incTop;
+        objRet['left'] += incLeft;
+        objRet["height"] = _pixelToMM(objRet["height"].toDouble());
+        //print(objRet);
 
         ret.add(objRet);
       }
@@ -96,16 +254,17 @@ class Report {
     }
   }
 
-  String _processText(String txt, var currData) {
+  String _processText(String txt, var data) {
+    //print(txt);
     txt = txt.trim();
     if (txt.substring(0, 1) == '[') {
       //value of field
       var field = txt.substring(1, (txt.length - 1));
-      return currData[field];
+      return data[field];
     } else {
       if (txt.indexOf('[') > 0) {
         String ret = txt;
-        currData.map((k, v) {
+        data.map((k, v) {
           if (k != null) ret = ret.replaceAll("[$k]", v);
           return MapEntry(k, v);
         });
@@ -114,5 +273,13 @@ class Report {
         return txt;
       }
     }
+  }
+
+  double _pixelToMM(double px) {
+    return px * 0.2645833;
+  }
+
+  double _mmToPixel(double mm) {
+    return mm / 0.2645833;
   }
 }
